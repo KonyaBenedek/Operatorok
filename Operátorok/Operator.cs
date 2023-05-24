a142241a51781ec417af0b0853ccc1679cc7231a
kbeni <benedekkonya005@gmail.com>
2023-05-24 14:45:01 +0200

    Add project files.

---
+++ Operátorok/Operator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operátorok
+{   
+    public class Operator
+    {
+        int aOperand;
+        int bOperand;
+        string operat;
+
+        public int AOperand { get => aOperand; set => aOperand = value; }
+        public int BOperand { get => bOperand; set => bOperand = value; }
+        public string Operat { get => operat; set => operat = value; }
+
+        public Operator(string sor) 
+        {
+            string[] adatok = sor.Split(' ');
+            aOperand = int.Parse(adatok[0].Trim());
+            operat = adatok[1].Trim();
+            bOperand = int.Parse(adatok[2].Trim());
+            
+        }   
+    }
+}
---
+++ Operátorok/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Operátorok;
+using System.Text;
+
+namespace Operátorok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Operator> operatorok = new List<Operator>();
+
+            StreamReader sw = new StreamReader("kifejezesek.txt", Encoding.UTF8);
+
+            while (!sw.EndOfStream)
+            {
+                operatorok.Add(new Operator(sw.ReadLine()));
+            }
+            sw.Close();
+
+            Console.WriteLine($"2. feladat: Kifejezések száma: {operatorok.Count}");
+
+            Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {operatorok.Count(x=> x.Operat == "mod")}");
+
+            int index = 0;
+            bool vanIlyen = false;
+            while (!vanIlyen)
+            {
+                vanIlyen = operatorok[index].AOperand % 10 == 0 && operatorok[index].BOperand % 10 == 0;
+                index++;
+            }
+            
+            Console.WriteLine($"4. feladat: {(vanIlyen ? "Van" : "Nincs")} ilyen kifejezés!");
+
+            Console.WriteLine("5. feladat: Statisztika");
+            operatorok.Where(x => x.Operat == "+" || x.Operat == "-" || x.Operat == "*" || x.Operat == "/" || x.Operat == "div" || x.Operat == "mod").GroupBy(x => x.Operat).ToList().ForEach(y => Console.WriteLine($"{y.Key} -> {y.Count()} db"));
+        }
+    }
+}
